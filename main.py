1b4fc052f3c70fd1fda39ab6cca73d1241bed333
Gsteil92 <steilga92@gmail.com>
2024-10-15 08:47:36 -0500

    changed program to now tell the amount of words in the book

--- main.py
+++ main.py
@@ -1,12 +1,31 @@
+"""
+Project Name: Bookbot
+Author: George Steil
+Creation Date: 10/14/2024
+"""
+
+# Global Variables
+book_path = "books/frankenstein.txt"
+test_path = "books/test_count.txt"
+
 def main():
-    book_path = "books/frankenstein.txt"
     text = get_book_text(book_path)
-    print(text)
+    count_of_words = word_count(text)
+    print(count_of_words)
 
 
 def get_book_text(book_path):
     with open(book_path) as f:
         return f.read()
+
+
+def word_count(text):
+    words = text.split()
+    w = 0
+    while w < len(words):
+        w = w + 1
+
+    return w
 
 
 
